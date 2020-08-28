4ab637246103f737fb4f82b84a83330beafcdfce
precisionbusiness <precisionbusiness888@gmail.com>
2020-08-28 11:18:29 +1000

    added

---
+++ scripts.js
@@ -0,0 +1,20 @@
+        function openNav() {
+            document.getElementById("menu").style.left = "0vw";
+            document.getElementById("languagebar").style.display = "none";
+
+          }
+  
+  
+          function closeNav() {
+            document.getElementById("menu").style.left = "100vw";
+            document.getElementById("languagebar").style.display = "block";
+          }
+
+
+
+
+
+
+
+          
+   
